8c93e07fc9da57c0b707c5bdb83b3d5b8c475ca9
Linus <linus.a.garcia@gmail.com>
2026-02-01 00:40:26 -0500

    Initial Skeleton and Hello World!

---
+++ build/CMakeFiles/telemetry_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/telemetry_engine.dir/src/main.cpp.o"
+  "CMakeFiles/telemetry_engine.dir/src/main.cpp.o.d"
+  "telemetry_engine"
+  "telemetry_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/telemetry_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//using namespace std; (makes text listing easier)
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
